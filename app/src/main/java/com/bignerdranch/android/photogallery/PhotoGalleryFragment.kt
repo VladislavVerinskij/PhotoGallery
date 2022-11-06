0e57f7ebc0e6bfaf1362bef5800a45fb42c33c79
Vladislav <vladislav.verinskij@gmail.com>
2022-11-06 11:38:21 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,44 @@
+package com.bignerdranch.android.photogallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.photogallery.api.FlickrApi
+import retrofit2.Retrofit
+import retrofit2.create
+
+class PhotoGalleryFragment : Fragment()  {
+
+    private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .build()
+
+        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+
+        photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
+        photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
+
+        return view
+    }
+
+    companion object {
+        fun newInstance() = PhotoGalleryFragment()
+    }
+}
