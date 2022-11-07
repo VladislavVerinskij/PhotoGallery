b182be3881afc2002af1379c0207103200f66803
Vladislav <vladislav.verinskij@gmail.com>
2022-11-07 16:08:28 +0300

    api

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,15 +1,20 @@
 package com.bignerdranch.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.api.FlickrApi
-import retrofit2.Retrofit
-import retrofit2.create
+import retrofit2.*
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment()  {
 
@@ -18,11 +23,12 @@ class PhotoGalleryFragment : Fragment()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com/")
-            .build()
-
-        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchPhotos()
+        flickrLiveData.observe(
+            this,
+            Observer { responseString ->
+                Log.d(TAG, "Response received: $responseString")
+            })
     }
 
     override fun onCreateView(
--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -5,6 +5,12 @@ import retrofit2.http.GET
 
 interface FlickrApi {
 
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=0c4ea72ad7005c6f05d5b39c633d0e20" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<String>
 }
